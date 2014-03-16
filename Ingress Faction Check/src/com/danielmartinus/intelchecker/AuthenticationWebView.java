0ee455608e84068963726e3966b43303501c2fb0
Daniel Martinus <dionsegijn@hotmail.com>
2014-03-16 11:43:04 +0100

    add setFitInScreen method

--- Ingress Faction Check/src/com/danielmartinus/intelchecker/AuthenticationWebView.java
+++ Ingress Faction Check/src/com/danielmartinus/intelchecker/AuthenticationWebView.java
@@ -1,11 +1,31 @@
 package com.danielmartinus.intelchecker;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class AuthenticationWebView extends WebView {
 
+    private WebSettings mSettings;
+    
+	private void initWebView(Context ctx) {
+		if(isInEditMode()) return;
+		 mSettings = getSettings();
+	     mSettings.setJavaScriptEnabled(true);
+	     mSettings.setDomStorageEnabled(true);
+	     mSettings.setAllowFileAccess(true);
+	     mSettings.setGeolocationEnabled(true);
+	     mSettings.setAppCacheEnabled(true);
+	     mSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+	     mSettings.setAppCachePath(getContext().getCacheDir().getAbsolutePath());
+	     mSettings.setDatabasePath(getContext().getApplicationInfo().dataDir + "/databases/");
+	     
+	     //TODO: add javascript interface
+	}
+	
 	// extended webview constructors //
 	public AuthenticationWebView(Context context) {
 		super(context);
@@ -20,5 +40,15 @@ public class AuthenticationWebView extends WebView {
         super(context, attrs, defStyle);
     }
     // end of webview constructors //
+    
+    public void setViewFitInScreen(boolean fitInScreen) {
+    	if(!fitInScreen) return;
+    	
+    	setInitialScale(1);
+    	getSettings().setLoadWithOverviewMode(true);
+    	getSettings().setUseWideViewPort(true);
+    	setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+    	setScrollbarFadingEnabled(false);
+    }
     
 }
