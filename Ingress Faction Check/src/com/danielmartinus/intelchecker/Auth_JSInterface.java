656a14a5ba6c21102e2a70e83220f30cddc953a4
Daniel Martinus <dionsegijn@hotmail.com>
2014-03-16 11:47:26 +0100

    add seperate JavascriptInterface

---
+++ Ingress Faction Check/src/com/danielmartinus/intelchecker/Auth_JSInterface.java
@@ -0,0 +1,23 @@
+package com.danielmartinus.intelchecker;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+public class Auth_JSInterface {
+
+	private Context context;
+	
+	Auth_JSInterface(Context ctx) {
+		this.context = ctx;
+	}
+	
+	
+	@JavascriptInterface
+	public void sendToAndroid(String text) {
+		Toast t = Toast.makeText(context, text, 2000);
+		t.show();
+	}
+	
+	//More javascript methods between webview and native here...	
+}
