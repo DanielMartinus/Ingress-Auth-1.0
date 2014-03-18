e9dba87a78493564754fc54b3de025dbba9b6a50
Dion Segijn <dionsegijn@hotmail.com>
2014-03-18 15:05:30 +0100

    initialize components

--- Ingress Faction Check/src/com/danielmartinus/intelchecker/IntelManager.java
+++ Ingress Faction Check/src/com/danielmartinus/intelchecker/IntelManager.java
@@ -5,9 +5,20 @@ import android.content.Context;
 public class IntelManager {
 
 	private Context context;
-	
-	public IntelManager(Context ctx) {
+	private AuthenticationWebView mWebView;
+	private AutoDeviceAuthentication mAuthentication;
+
+    public static String URL_INTEL = "https://www.ingress.com/intel/?vp=f";
+    
+	public IntelManager(Context ctx, AuthenticationWebView webview) {
 		this.context = ctx;
+		
+		mWebView = webview;
+		if(webview == null) return;
+		
+		//Custom webview client handling its own js injection
+		mWebView.setWebViewClient(new Auth_WebViewClient(getWebViewActivity()));
+		mWebView.setViewFitInScreen(true);
 	}
 	
 }
